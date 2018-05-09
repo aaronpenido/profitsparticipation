612526dd6338cd153f6448b305bd326eb8c0be11
Aaron Penido <aaron.penido@gmail.com>
2018-05-09 14:40:20 -0300

    Addd FileReader class

---
+++ src/main/java/models/io/FileReader.java
@@ -0,0 +1,35 @@
+package models.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FileReader implements IOReader {
+
+    private String filePath;
+
+    public FileReader(String filePath) {
+        this.filePath = filePath;
+    }
+
+    @Override
+    public String read() {
+        try {
+            Path path = Paths.get(filePath);
+
+            StringBuilder data = new StringBuilder();
+            Stream<String> lines = Files.lines(path);
+
+            lines.forEach(line -> data.append(line));
+            lines.close();
+
+            return data.toString();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        return null;
+    }
+}
---
+++ src/test/java/models/io/FileReaderTest.java
@@ -0,0 +1,25 @@
+package models.io;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FileReaderTest {
+
+    private String filePath;
+
+    @Before
+    public void setUp() {
+        filePath = getClass().getClassLoader().getResource("fileTest.txt").getPath();
+    }
+
+    @Test
+    public void readContentFromFile() {
+        FileReader fileReader = new FileReader(filePath);
+
+        String value = fileReader.read();
+
+        assertThat(value).isEqualTo("test");
+    }
+}
